d90247af754b6496f6ff615c78cff6320767fe6e
dsolonov <vladimirkz2020@gmail.com>
2025-05-27 11:20:20 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = 'geoguess-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
